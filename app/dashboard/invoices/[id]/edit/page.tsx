90c562af26e3734c809fb272673955be9a343f9d
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-10-01 12:45:21 +0500

    course_almost_completed

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,11 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers,fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
-
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Edit Invoice ',
+};
  
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
@@ -26,7 +30,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
           },
         ]}
       />
-      {/* <Form invoice={invoice} customers={customers} /> */}
+      <Form invoice={invoice} customers={customers} />
     </main>
   );
 }
